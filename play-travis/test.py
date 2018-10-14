3a7e2d5943ed7c0fa3bd756d9db3fe5495b4465c
Konstantin Ermakov <konstantin.ermakov@corp.mail.ru>
2018-10-14 20:56:21 +0300

    fix test.py

--- play-travis/test.py
+++ play-travis/test.py
@@ -1,9 +1,12 @@
 import unittest
 
+
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
         self.assertEqual(1 + 1, 1)
+        #test slack
+
 
 if __name__ == '__main__':
     unittest.main()
