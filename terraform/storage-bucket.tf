fa67b6044652fb3ed047f9bd30489ad0c8f0a49b
Konstantin Ermakov <konstantin.ermakov@corp.mail.ru>
2018-12-18 00:42:28 +0300

    fix

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -7,7 +7,7 @@ provider "google" {
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
-  name    = ["storage-bucket-one", "storage-bucket-two"]
+  name    = ["storage-bucket-my-one", "storage-bucket-my-two"]
 }
 
 output storage-bucket_url {
